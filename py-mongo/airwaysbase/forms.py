6f03d3fabf665f97196b2cb44958a517d72e14a0
prihodkoserzh <mastersnetru@gmail.com>
2016-12-05 01:54:40 +0200

    Use embedded docs model

--- py-mongo/airwaysbase/forms.py
+++ py-mongo/airwaysbase/forms.py
@@ -4,9 +4,9 @@ from .custom_models import Aircraft, Airport
 
 
 class NewFlightForm(forms.Form):
-    source_id = forms.ChoiceField(label='Source', choices=Airport.get_choices())
-    destination_id = forms.ChoiceField(label='Destination', choices=Airport.get_choices())
-    aircraft_id = forms.ChoiceField(label='Airplane', choices=Aircraft.get_choices())
+    source = forms.ChoiceField(label='Source', choices=Airport.get_choices())
+    destination = forms.ChoiceField(label='Destination', choices=Airport.get_choices())
+    aircraft = forms.ChoiceField(label='Airplane', choices=Aircraft.get_choices())
     start_date = forms.DateTimeField(widget=DateTimeWidget(
         attrs={'id':'start_date'}, usel10n=True, bootstrap_version=3))
     end_date = forms.DateTimeField(widget=DateTimeWidget(
